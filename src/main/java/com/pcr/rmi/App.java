e3d798adca3f1f6f67a56d6515a15ee0c7828e5f
Abdourahamane LY <abdourahamane.ly1@gmail.com>
2019-04-15 01:26:41 +0200

    Initial commit

---
+++ src/main/java/com/pcr/rmi/App.java
@@ -0,0 +1,13 @@
+package com.pcr.rmi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
