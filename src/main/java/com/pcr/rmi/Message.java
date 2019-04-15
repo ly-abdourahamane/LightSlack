cb2ff964f5d3d2d60f9ff4d56f5022c9ce12c2a9
Abdourahamane LY <abdourahamane.ly1@gmail.com>
2019-04-15 23:28:10 +0200

    Adding message object

--- src/main/java/com/pcr/rmi/Message.java
+++ src/main/java/com/pcr/rmi/Message.java
@@ -1,8 +1,23 @@
 package com.pcr.rmi;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 /**
  * @author abdou on 15/04/19.
  * @project lightslack
  */
+@Getter
+@Setter
+@ToString
 public class Message {
+
+    private String value;
+    private String sender;
+
+    public Message(String value, String sender) {
+        this.value = value;
+        this.sender = sender;
+    }
 }
