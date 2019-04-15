47d2752820d36d84b3ef21404f6a361eb11de6b8
Abdourahamane LY <abdourahamane.ly1@gmail.com>
2019-04-15 23:27:51 +0200

    Receive and get messages with the server

--- src/main/java/com/pcr/rmi/server/IServer.java
+++ src/main/java/com/pcr/rmi/server/IServer.java
@@ -1,8 +1,16 @@
 package com.pcr.rmi.server;
 
+import com.pcr.rmi.Message;
+
+import java.rmi.RemoteException;
+import java.util.List;
+
 /**
  * @author abdou on 15/04/19.
  * @project lightslack
  */
 public interface IServer {
+
+    void receive(Message message) throws RemoteException;
+    List<Message> getMessages() throws RemoteException;
 }
--- src/main/java/com/pcr/rmi/server/Server.java
+++ src/main/java/com/pcr/rmi/server/Server.java
@@ -1,8 +1,32 @@
 package com.pcr.rmi.server;
 
+import com.pcr.rmi.Message;
+
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author abdou on 15/04/19.
  * @project lightslack
  */
-public class Server {
+public class Server extends UnicastRemoteObject implements IServer {
+
+    private List<Message> messages;
+
+    protected Server() throws RemoteException {
+        super();
+        messages = new ArrayList<>();
+    }
+
+    @Override
+    public void receive(Message message) throws RemoteException {
+        messages.add(message);
+    }
+
+    @Override
+    public List<Message> getMessages() throws RemoteException {
+        return messages;
+    }
 }
